b7aa14d2e58761bc107495bd1be87ccce815b011
EdmondLeahy <edmondleahy@gmail.com>
2021-11-04 17:03:58 -0600

    candlestick working

--- binance_trade_bot/plotter.py
+++ binance_trade_bot/plotter.py
@@ -2,13 +2,13 @@ import plotly.graph_objects as go
 import pandas as pd
 from datetime import datetime, timedelta
 from binance.client import Client
-
+#
 import dash
 from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
-
-# LOOK AT THIS : https://www.youtube.com/watch?v=37Zj955LFT0
+#
+# # LOOK AT THIS : https://www.youtube.com/watch?v=37Zj955LFT0
 
 api_key = 'rxjvvkGRGUcApapeWzUFAvJhHe3lC3doIEbfqL5v75xK4Wwo2H8MiQOoYdx1lwdI'
 api_secret_key = 'GFE02ezysvl44wWjdjL05YnpxQqz9lgGir6HEc9rcq10f0cTHSufF6kw98120Btr'
@@ -56,22 +56,22 @@ def add_h_line(fig, data):
 
     fig.add_hline(y=current_price, line_width=3, line_dash="dash", line_color="green",
                   annotation_text=f'{current_price}')
-
+    # fig.update()
     return fig
 
-def make_layout(data):
+def make_layout(data, fig):
 
     first_date = min(data['dateTime'])
     last_date = max(data['dateTime'])
     lowest_val= float(min(data['low']))
     highest_val= float(max(data['high']))
 
-    layout = go.Layout(xaxis=dict(range=[first_date, last_date+timedelta(minutes=10)]),
+    fig.update_layout(xaxis=dict(range=[first_date, last_date+timedelta(minutes=10)]),
                        yaxis=dict(range=[lowest_val, highest_val]),
                        title=f'{datetime.now()}'
                        )
 
-    return layout
+    return fig
 
 
 
@@ -79,20 +79,19 @@ def make_layout(data):
 
 @app.callback(Output('live-graph', 'figure'),
               [Input('graph-update', 'n_intervals')])
-def update_graph_scatter(n, time_span=1):
-    candle_data = get_historical_data(time_span)
-
+def graph_update(_):
+    candle_data = get_historical_data(3)
     fig = go.Figure(go.Candlestick(x=candle_data['dateTime'],
                           open=candle_data['open'],
                           high=candle_data['high'],
                           low=candle_data['low'],
                           close=candle_data['close']))
 
-    fig2 = add_h_line(go.Figure(), candle_data)
-    layout = make_layout(candle_data)
+    fig = add_h_line(fig, candle_data)
+    fig = make_layout(candle_data, fig)
 
-    return {'data': [fig.data[0], fig2], 'layout' : layout}
-
+    return fig
+#
 def run_plot_server():
     app.run_server(debug=True, port=8051)
 
